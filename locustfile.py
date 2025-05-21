60ae23ab84d7f333fb2a2b14ad6d421be6ef4377
Vishvambruth J T <vishruth.cse@gmail.com>
2025-05-22 00:49:00 +0530

    Improved framework structure

--- locustfile.py
+++ locustfile.py
@@ -1,9 +1,9 @@
 from locust import events
-from users.blaze_demo_user_taskset import BlazeDemoTaskSetUserClass
-from users.json_placeholder_user_taskset import JsonPlaceholderTaskSetUserClass
+from users.blaze_demo_user_taskset import BlazeDemoUserTaskSetUserClass
+from users.json_placeholder_user_taskset import JsonPlaceholderUserTaskSetUserClass
 
 # Optionally define a list of user classes
-user_classes = [BlazeDemoTaskSetUserClass, JsonPlaceholderTaskSetUserClass]
+user_classes = [BlazeDemoUserTaskSetUserClass, JsonPlaceholderUserTaskSetUserClass]
 
 @events.init.add_listener
 def on_locust_init(environment, **kwargs):
